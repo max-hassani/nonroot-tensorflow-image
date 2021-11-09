2a7c803177a11076b7cee878c4cbb1dd9bab669b
muh-hassani <mhassani86@gmail.com>
2021-11-09 21:25:31 +0100

    fixing lack of NB_UID

--- start.sh
+++ start.sh
@@ -44,22 +44,22 @@ if [ "$(id -u)" == 0 ] ; then
 
     # Only attempt to change the jovyan username if it exists
     if id jovyan &> /dev/null ; then
-        echo "Set username to: ${NB_USER}"
-        usermod -d "/home/${NB_USER}" -l "${NB_USER}" jovyan
+        echo "Set username to: ${DOCKER_USER}"
+        usermod -d "/home/${DOCKER_USER}" -l "${DOCKER_USER}" jovyan
     fi
 
     # handle home and working directory if the username changed
-    if [[ "${NB_USER}" != "jovyan" ]]; then
+    if [[ "${DOCKER_USER}" != "jovyan" ]]; then
         # changing username, make sure homedir exists
         # (it could be mounted, and we shouldn't create it if it already exists)
-        if [[ ! -e "/home/${NB_USER}" ]]; then
-            echo "Copying home dir to /home/${NB_USER}"
-            mkdir "/home/${NB_USER}"
-            cp -a /home/jovyan/. "/home/${NB_USER}/" || ln -s /home/jovyan "/home/${NB_USER}"
+        if [[ ! -e "/home/${DOCKER_USER}" ]]; then
+            echo "Copying home dir to /home/${DOCKER_USER}"
+            mkdir "/home/${DOCKER_USER}"
+            cp -a /home/jovyan/. "/home/${DOCKER_USER}/" || ln -s /home/jovyan "/home/${DOCKER_USER}"
         fi
-        # if workdir is in /home/jovyan, cd to /home/${NB_USER}
+        # if workdir is in /home/jovyan, cd to /home/${DOCKER_USER}
         if [[ "${PWD}/" == "/home/jovyan/"* ]]; then
-            newcwd="/home/${NB_USER}/${PWD:13}"
+            newcwd="/home/${DOCKER_USER}/${PWD:13}"
             echo "Setting CWD to ${newcwd}"
             cd "${newcwd}"
         fi
@@ -68,44 +68,44 @@ if [ "$(id -u)" == 0 ] ; then
     # Handle case where provisioned storage does not have the correct permissions by default
     # Ex: default NFS/EFS (no auto-uid/gid)
     if [[ "${CHOWN_HOME}" == "1" || "${CHOWN_HOME}" == 'yes' ]]; then
-        echo "Changing ownership of /home/${NB_USER} to ${NB_UID}:${NB_GID} with options '${CHOWN_HOME_OPTS}'"
+        echo "Changing ownership of /home/${DOCKER_USER} to ${DOCKER_UID}:${DOCKER_GID} with options '${CHOWN_HOME_OPTS}'"
         # shellcheck disable=SC2086
-        chown ${CHOWN_HOME_OPTS} "${NB_UID}:${NB_GID}" "/home/${NB_USER}"
+        chown ${CHOWN_HOME_OPTS} "${DOCKER_UID}:${DOCKER_GID}" "/home/${DOCKER_USER}"
     fi
     if [ -n "${CHOWN_EXTRA}" ]; then
         for extra_dir in $(echo "${CHOWN_EXTRA}" | tr ',' ' '); do
-            echo "Changing ownership of ${extra_dir} to ${NB_UID}:${NB_GID} with options '${CHOWN_EXTRA_OPTS}'"
+            echo "Changing ownership of ${extra_dir} to ${DOCKER_UID}:${DOCKER_GID} with options '${CHOWN_EXTRA_OPTS}'"
             # shellcheck disable=SC2086
-            chown ${CHOWN_EXTRA_OPTS} "${NB_UID}:${NB_GID}" "${extra_dir}"
+            chown ${CHOWN_EXTRA_OPTS} "${DOCKER_UID}:${DOCKER_GID}" "${extra_dir}"
         done
     fi
 
-    # Change UID:GID of NB_USER to NB_UID:NB_GID if it does not match
-    if [ "${NB_UID}" != "$(id -u "${NB_USER}")" ] || [ "${NB_GID}" != "$(id -g "${NB_USER}")" ]; then
-        echo "Set user ${NB_USER} UID:GID to: ${NB_UID}:${NB_GID}"
-        if [ "${NB_GID}" != "$(id -g "${NB_USER}")" ]; then
-            groupadd -f -g "${NB_GID}" -o "${NB_GROUP:-${NB_USER}}"
+    # Change UID:GID of DOCKER_USER to DOCKER_UID:DOCKER_GID if it does not match
+    if [ "${DOCKER_UID}" != "$(id -u "${DOCKER_USER}")" ] || [ "${DOCKER_GID}" != "$(id -g "${DOCKER_USER}")" ]; then
+        echo "Set user ${DOCKER_USER} UID:GID to: ${DOCKER_UID}:${DOCKER_GID}"
+        if [ "${DOCKER_GID}" != "$(id -g "${DOCKER_USER}")" ]; then
+            groupadd -f -g "${DOCKER_GID}" -o "${NB_GROUP:-${DOCKER_USER}}"
         fi
-        userdel "${NB_USER}"
-        useradd --home "/home/${NB_USER}" -u "${NB_UID}" -g "${NB_GID}" -G 100 -l "${NB_USER}"
+        userdel "${DOCKER_USER}"
+        useradd --home "/home/${DOCKER_USER}" -u "${DOCKER_UID}" -g "${DOCKER_GID}" -G 100 -l "${DOCKER_USER}"
     fi
 
     # Enable sudo if requested
     if [[ "${GRANT_SUDO}" == "1" || "${GRANT_SUDO}" == 'yes' ]]; then
-        echo "Granting ${NB_USER} sudo access and appending ${CONDA_DIR}/bin to sudo PATH"
-        echo "${NB_USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
+        echo "Granting ${DOCKER_USER} sudo access and appending ${CONDA_DIR}/bin to sudo PATH"
+        echo "${DOCKER_USER} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
     fi
 
     # Add ${CONDA_DIR}/bin to sudo secure_path
     sed -r "s#Defaults\s+secure_path\s*=\s*\"?([^\"]+)\"?#Defaults secure_path=\"\1:${CONDA_DIR}/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
 
-    # Exec the command as NB_USER with the PATH and the rest of
+    # Exec the command as DOCKER_USER with the PATH and the rest of
     # the environment preserved
     run-hooks /usr/local/bin/before-notebook.d
     echo "Executing the command:" "${cmd[@]}"
-    exec sudo -E -H -u "${NB_USER}" PATH="${PATH}" XDG_CACHE_HOME="/home/${NB_USER}/.cache" PYTHONPATH="${PYTHONPATH:-}" "${cmd[@]}"
+    exec sudo -E -H -u "${DOCKER_USER}" PATH="${PATH}" XDG_CACHE_HOME="/home/${DOCKER_USER}/.cache" PYTHONPATH="${PYTHONPATH:-}" "${cmd[@]}"
 else
-    if [[ "${NB_UID}" == "$(id -u jovyan 2>/dev/null)" && "${NB_GID}" == "$(id -g jovyan 2>/dev/null)" ]]; then
+    if [[ "${DOCKER_UID}" == "$(id -u jovyan 2>/dev/null)" && "${DOCKER_GID}" == "$(id -g jovyan 2>/dev/null)" ]]; then
         # User is not attempting to override user/group via environment
         # variables, but they could still have overridden the uid/gid that
         # container runs as. Check that the user has an entry in the passwd
@@ -130,11 +130,11 @@ else
     else
         # Warn if looks like user want to override uid/gid but hasn't
         # run the container as root.
-        if [[ -n "${NB_UID}" && "${NB_UID}" != "$(id -u)" ]]; then
-            echo "Container must be run as root to set NB_UID to ${NB_UID}"
+        if [[ -n "${DOCKER_UID}" && "${DOCKER_UID}" != "$(id -u)" ]]; then
+            echo "Container must be run as root to set DOCKER_UID to ${DOCKER_UID}"
         fi
-        if [[ -n "${NB_GID}" && "${NB_GID}" != "$(id -g)" ]]; then
-            echo "Container must be run as root to set NB_GID to ${NB_GID}"
+        if [[ -n "${DOCKER_GID}" && "${DOCKER_GID}" != "$(id -g)" ]]; then
+            echo "Container must be run as root to set DOCKER_GID to ${DOCKER_GID}"
         fi
     fi
 
